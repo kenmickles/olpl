faef765308dd25556a75ad04e2edcbe2857dd8aa
Ken Mickles <ken@37i.net>
2012-05-11 11:46:41 -0400

    add fork me button

--- index.php
+++ index.php
@@ -130,6 +130,7 @@ if ( mysql_num_rows($result) ) {
       </form>
     </div>
   </div>
+	<a href="https://github.com/kenmickles/olpl"><img style="position: absolute; top: 0; right: 0; border: 0; z-index: 1100" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
 </body>
 </html>
   
